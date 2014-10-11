c40a7b8c30e393c400b27f75a0dbfc8209db51d5
Nick Sagona <nick@moc10media.com>
2014-10-11 17:12:53 -0500

    Finish Imagick Draw features

--- src/Image/Draw/Imagick.php
+++ src/Image/Draw/Imagick.php
@@ -100,6 +100,57 @@ class Imagick extends AbstractDraw
     }
 
     /**
+     * Draw a rounded rectangle on the image.
+     *
+     * @param  int $x
+     * @param  int $y
+     * @param  int $w
+     * @param  int $h
+     * @param  int $rx
+     * @param  int $ry
+     * @return Imagick
+     */
+    public function roundRectangle($x, $y, $w, $h = null, $rx = 10, $ry = null)
+    {
+        $x2 = $x + $w;
+        $y2 = $y + ((null === $h) ? $w : $h);
+        if (null === $ry) {
+            $ry = $rx;
+        }
+
+        $draw = new \ImagickDraw();
+
+        if (null !== $this->fillColor) {
+            $draw->setFillColor($this->image->getColor($this->fillColor, $this->opacity));
+        }
+
+        if ($this->strokeWidth > 0) {
+            $draw->setStrokeColor($this->image->getColor($this->strokeColor, $this->opacity));
+            $draw->setStrokeWidth($this->strokeWidth);
+        }
+
+        $draw->roundRectangle($x, $y, $x2, $y2, $rx, $ry);
+        $this->image->resource()->drawImage($draw);
+
+        return $this;
+    }
+
+    /**
+     * Draw a rounded square on the image.
+     *
+     * @param  int $x
+     * @param  int $y
+     * @param  int $w
+     * @param  int $rx
+     * @param  int $ry
+     * @return Imagick
+     */
+    public function roundSquare($x, $y, $w, $rx = 10, $ry = null)
+    {
+        return $this->roundRectangle($x, $y, $w, $w, $rx, $ry);
+    }
+
+    /**
      * Draw an ellipse on the image.
      *
      * @param  int $x
@@ -163,10 +214,11 @@ class Imagick extends AbstractDraw
         $hgt = (null === $h) ? $w : $h;
 
         $draw = new \ImagickDraw();
+        $draw->setFillOpacity(0);
         $draw->setStrokeColor($this->image->getColor($this->strokeColor, $this->opacity));
         $draw->setStrokeWidth($this->strokeWidth);
 
-        $draw->arc($x, $y, $x + $wid, $y + $hgt, $start, $end);
+        $draw->ellipse($x, $y, $wid, $hgt, $start, $end);
 
         $this->image->resource()->drawImage($draw);
 
@@ -190,19 +242,82 @@ class Imagick extends AbstractDraw
         $hgt = (null === $h) ? $w : $h;
 
         $draw = new \ImagickDraw();
-        if (null !== $this->fillColor) {
-            $draw->setFillColor($this->image->getColor($this->fillColor, $this->opacity));
-        }
-        if ($this->strokeWidth > 0) {
-            $draw->setStrokeColor($this->image->getColor($this->strokeColor, $this->opacity));
-            $draw->setStrokeWidth($this->strokeWidth);
-        }
+        $draw->setFillColor($this->image->getColor($this->fillColor));
+
+        $x1 = $w * cos($start / 180 * pi());
+        $y1 = $h * sin($start / 180 * pi());
+        $x2 = $w * cos($end / 180 * pi());
+        $y2 = $h * sin($end / 180 * pi());
 
         $draw->ellipse($x, $y, $wid, $hgt, $start, $end);
-
         $this->image->resource()->drawImage($draw);
 
+        if ($this->strokeWidth > 0) {
+            $draw = new \ImagickDraw();
+
+            $draw->setFillColor($this->image->getColor($this->fillColor));
+            $draw->setStrokeColor($this->image->getColor($this->strokeColor));
+            $draw->setStrokeWidth($this->strokeWidth);
+
+            $draw->ellipse($x, $y, $wid, $hgt, $start, $end);
+            $draw->line($x + $x1, $y + $y1, $x + $x2, $y + $y2);
+
+            $this->image->resource()->drawImage($draw);
+        }
+
         return $this;
+    }
+
+    /**
+     * Draw a pie slice on the image.
+     *
+     * @param  int $x
+     * @param  int $y
+     * @param  int $start
+     * @param  int $end
+     * @param  int $w
+     * @param  int $h
+     * @return Imagick
+     */
+    public function pie($x, $y, $start, $end, $w, $h = null)
+    {
+
+        $wid = $w;
+        $hgt = (null === $h) ? $w : $h;
+
+        $draw = new \ImagickDraw();
+        $draw->setFillColor($this->image->getColor($this->fillColor));
+
+        $x1 = $w * cos($start / 180 * pi());
+        $y1 = $h * sin($start / 180 * pi());
+        $x2 = $w * cos($end / 180 * pi());
+        $y2 = $h * sin($end / 180 * pi());
+
+        $points = [
+            ['x' => $x, 'y' => $y],
+            ['x' => $x + $x1, 'y' => $y + $y1],
+            ['x' => $x + $x2, 'y' => $y + $y2]
+        ];
+
+        $draw->polygon($points);
+
+        $draw->ellipse($x, $y, $wid, $hgt, $start, $end);
+        $this->image->resource()->drawImage($draw);
+
+        if ($this->strokeWidth > 0) {
+            $draw = new \ImagickDraw();
+
+            $draw->setFillColor($this->image->getColor($this->fillColor));
+            $draw->setStrokeColor($this->image->getColor($this->strokeColor));
+            $draw->setStrokeWidth($this->strokeWidth);
+
+            $draw->ellipse($x, $y, $wid, $hgt, $start, $end);
+            $draw->line($x, $y, $x + $x1, $y + $y1);
+            $draw->line($x, $y, $x + $x2, $y + $y2);
+
+            $this->image->resource()->drawImage($draw);
+        }
+
         return $this;
     }
 
