18195d12eb8c4f7bfb79fa98c7fbd38d9a7a030b
Nick Sagona <nick@nolainteractive.com>
2015-05-30 15:48:07 -0500

    Separate recursion loop test out

--- tests/ServiceTest.php
+++ tests/ServiceTest.php
@@ -148,18 +148,4 @@ class ServiceTest extends \PHPUnit_Framework_TestCase
         $this->assertNull($test6->foo);
     }
 
-    public function testRecursionLoop()
-    {
-        $this->setExpectedException('Pop\Service\Exception');
-        $services = new Locator();
-        $services->set('service1', function($locator) {
-            return $locator->get('service2');
-        });
-        $services->set('service2', function($locator) {
-            return $locator->get('service1');
-        });
-
-        $result = $services->get('service1');
-    }
-
 }
