f125e45cb9b4c75ded25561701910f3973710f49
Nick Sagona <nick@nolainteractive.com>
2015-07-29 16:03:47 -0500

    Update version number

--- src/Router/Match/MatchInterface.php
+++ src/Router/Match/MatchInterface.php
@@ -21,7 +21,7 @@ namespace Pop\Router\Match;
  * @author     Nick Sagona, III <dev@nolainteractive.com>
  * @copyright  Copyright (c) 2009-2015 NOLA Interactive, LLC. (http://www.nolainteractive.com)
  * @license    http://www.popphp.org/license     New BSD License
- * @version    2.0.0
+ * @version    2.0.1
  */
 interface MatchInterface
 {
