a831161f494179849993a3a6d638e142a0789796
Nick Sagona <nick@nolainteractive.com>
2015-07-30 11:49:23 -0500

    Correct version

--- src/Router/Exception.php
+++ src/Router/Exception.php
@@ -21,6 +21,6 @@ namespace Pop\Router;
  * @author     Nick Sagona, III <dev@nolainteractive.com>
  * @copyright  Copyright (c) 2009-2015 NOLA Interactive, LLC. (http://www.nolainteractive.com)
  * @license    http://www.popphp.org/license     New BSD License
- * @version    2.0.1
+ * @version    2.0.0
  */
 class Exception extends \Exception {}
