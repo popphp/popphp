9792465c5b87548038d601353210e7469e908f7c
Nick Sagona <nick@nolainteractive.com>
2019-12-27 12:56:52 -0600

    Add pop-utils

--- src/Model/AbstractModel.php
+++ src/Model/AbstractModel.php
@@ -13,6 +13,8 @@
  */
 namespace Pop\Model;
 
+use Pop\Utils;
+
 /**
  * Pop abstract model class
  *
@@ -23,145 +25,7 @@ namespace Pop\Model;
  * @license    http://www.popphp.org/license     New BSD License
  * @version    3.3.3
  */
-abstract class AbstractModel implements \ArrayAccess, \Countable, \IteratorAggregate
+abstract class AbstractModel extends Utils\ArrayObject
 {
-
-    /**
-     * Model data array
-     * @var array
-     */
-    protected $data = [];
-
-    /**
-     * Constructor
-     *
-     * Instantiate a model object
-     *
-     * @param  array $data
-     */
-    public function __construct(array $data = [])
-    {
-        $this->data = $data;
-    }
-
-    /**
-     * Method to get the count of items in the model
-     *
-     * @return int
-     */
-    public function count()
-    {
-        return count($this->data);
-    }
-
-    /**
-     * Method to iterate over the data
-     *
-     * @return \ArrayIterator
-     */
-    public function getIterator()
-    {
-        return new \ArrayIterator($this->data);
-    }
-
-    /**
-     * Return all model data as an array
-     *
-     * @return array
-     */
-    public function toArray()
-    {
-        return $this->data;
-    }
-
-    /**
-     * Magic get method to return the value of data[$name].
-     *
-     * @param  string $name
-     * @return mixed
-     */
-    public function __get($name)
-    {
-        return (array_key_exists($name, $this->data)) ? $this->data[$name] : null;
-    }
-
-    /**
-     * Magic set method to set the property to the value of data[$name].
-     *
-     * @param  string $name
-     * @param  mixed  $value
-     * @return void
-     */
-    public function __set($name, $value)
-    {
-        $this->data[$name] = $value;
-    }
-
-    /**
-     * Return the isset value of data[$name].
-     *
-     * @param  string $name
-     * @return boolean
-     */
-    public function __isset($name)
-    {
-        return isset($this->data[$name]);
-    }
-
-    /**
-     * Unset data[$name].
-     *
-     * @param  string $name
-     * @return void
-     */
-    public function __unset($name)
-    {
-        unset($this->data[$name]);
-    }
-
-    /**
-     * ArrayAccess offsetExists
-     *
-     * @param  mixed $offset
-     * @return boolean
-     */
-    public function offsetExists($offset)
-    {
-        return $this->__isset($offset);
-    }
-
-    /**
-     * ArrayAccess offsetGet
-     *
-     * @param  mixed $offset
-     * @return mixed
-     */
-    public function offsetGet($offset)
-    {
-        return $this->__get($offset);
-    }
-
-    /**
-     * ArrayAccess offsetSet
-     *
-     * @param  mixed $offset
-     * @param  mixed $value
-     * @return void
-     */
-    public function offsetSet($offset, $value)
-    {
-        $this->__set($offset, $value);
-    }
-
-    /**
-     * ArrayAccess offsetUnset
-     *
-     * @param  mixed $offset
-     * @return void
-     */
-    public function offsetUnset($offset)
-    {
-        $this->__unset($offset);
-    }
 
 }
