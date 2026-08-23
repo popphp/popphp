eabfc51f8ef309c8afba4564b1dc9dbf15597f15
Nick Sagona <nick@noladev.com>
2026-08-23 11:39:07 -0500

    Update email, license and copyrights

--- src/AbstractApplication.php
+++ src/AbstractApplication.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
  * Pop PHP Framework (https://www.popphp.org/)
  *
  * @link       https://github.com/popphp/popphp-framework
- * @author     Nick Sagona, III <dev@noladev.com>
- * @copyright  Copyright (c) 2009-2027 NOLA Interactive, LLC.
+ * @author     Nick Sagona, III <nick@popphp.org>
+ * @copyright  Copyright (c) 2009-2026 Nick Sagona, III
  * @license    https://www.popphp.org/license     New BSD License
  */
 
@@ -22,8 +22,8 @@ use InvalidArgumentException;
  *
  * @category   Pop
  * @package    Pop
- * @author     Nick Sagona, III <dev@noladev.com>
- * @copyright  Copyright (c) 2009-2027 NOLA Interactive, LLC.
+ * @author     Nick Sagona, III <nick@popphp.org>
+ * @copyright  Copyright (c) 2009-2026 Nick Sagona, III
  * @license    https://www.popphp.org/license     New BSD License
  * @version    5.0.0
  */
